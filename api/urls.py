70e7a17c58d1906fd4768bca92619e19f893678e
rla_thdus <0228ksy@naver.com>
2023-03-07 22:15:20 +0900

    Feat: DRF 예시 코드를 view, serializer 등의 구조로 분리

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from api.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
