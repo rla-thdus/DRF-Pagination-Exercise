882c6b452fb94900d912fa033a632fd14300f0ee
rla_thdus <0228ksy@naver.com>
2023-03-08 20:46:54 +0900

    Feat: 카테고리 모델 생성

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple one-line text.')
+
+    def __str__(self):
+        return self.name
