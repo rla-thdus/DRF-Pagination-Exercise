80194b52ad8f30a12e14c6fc67e82c57cb4b3ce0
rla_thdus <0228ksy@naver.com>
2023-03-08 20:59:09 +0900

    Feat: 게시물 crud modelViewSet 사용해서 구현

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import UserViewSet
+from api.views import UserViewSet, PostViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from api.serializers import UserSerializer
+from api.models import Post
+from api.serializers import UserSerializer, PostSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
