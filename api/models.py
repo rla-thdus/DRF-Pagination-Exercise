ad806c580581424c58f41c5b55acbaae691b7a6e
rla_thdus <0228ksy@naver.com>
2023-03-08 20:48:19 +0900

    Feat: 게시물 모델 생성

--- api/models.py
+++ api/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 
+class Post(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, blank=True, null=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    title = models.CharField('TITLE', max_length=50)
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple one-line text.')
+    image = models.ImageField('IMAGE', upload_to='blog/%Y/%m/', blank=True, null=True)
+    content = models.TextField('CONTENT')
+    create_dt = models.DateTimeField('CREATE DT', auto_now_add=True)
+    update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
+    like = models.PositiveSmallIntegerField('LIKE', default=0)
+
+    class Meta:
+        ordering = ('update_dt',)
+
+    def __str__(self):
+        return self.title
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple one-line text.')
