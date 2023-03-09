b49abc10d4e5c69456ceacfefa5560ba7f61ea4d
rla_thdus <0228ksy@naver.com>
2023-03-09 21:43:33 +0900

    Feat: 좋아요 업데이트 시킬 때 응답으로 좋아요 개수만 보내게 수정

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 
@@ -32,7 +33,7 @@ class PostLikeAPIView(UpdateAPIView):
         if getattr(instance, '_prefetched_objects_cache', None):
             instance._prefetched_objects_cache = {}
 
-        return Response(serializer.data)
+        return Response(serializer.data['like'])
 
 
 class TagViewSet(viewsets.ModelViewSet):
