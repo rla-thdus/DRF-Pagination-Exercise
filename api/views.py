e545d5cf2bef73c0c2c1fcbf401abf76ceb331d5
rla_thdus <0228ksy@naver.com>
2023-03-11 11:59:48 +0900

    Feat: 좋아요 누르는 기능 patch에서 get 메소드로 호출하게 변경

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.generics import UpdateAPIView
+from rest_framework.generics import UpdateAPIView, GenericAPIView
 from rest_framework.response import Response
 
 from api.models import Post, Tag, Category, Comment
@@ -18,22 +17,14 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
 
-class PostLikeAPIView(UpdateAPIView):
+class PostLikeAPIView(GenericAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+    def get(self, request, *args, **kwargs):
         instance = self.get_object()
-        data = {'like': instance.like + 1}
-        serializer = self.get_serializer(instance, data=data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data['like'])
+        instance.like += 1
+        instance.save()
+        return Response(instance.like)
 
 
 class TagViewSet(viewsets.ModelViewSet):
