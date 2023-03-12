545ea78f64086ac3d174aa028985899f0589bcfd
rla_thdus <0228ksy@naver.com>
2023-03-12 11:20:47 +0900

    Style: 사용되지 않는 UpdateAPIView 임포트 제거

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.generics import UpdateAPIView, GenericAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from api.models import Post, Tag, Category, Comment
