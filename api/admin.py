32afa8eca21bb8bda2eea9f7b815aab2f65014f6
rla_thdus <0228ksy@naver.com>
2023-03-21 20:57:11 +0900

    Feat: 게시물 어드민 설정

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'category', 'title', 'description', 'image', 'content', 'like']
