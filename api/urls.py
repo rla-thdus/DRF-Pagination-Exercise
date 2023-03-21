ad14db0941b3e93f9f979e4b77cc7de8a5565064
rla_thdus <0228ksy@naver.com>
2023-03-21 21:04:31 +0900

    refactor: 페이지네이션 관련 없는 코드 정리

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,8 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from api import views
-from api.views import UserViewSet, PostViewSet, TagViewSet, CategoryViewSet, CommentViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'posts', PostViewSet)
-router.register(r'tags', TagViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('posts/', views.PostListAPIView.as_view(), name='post-list'),
-    path('posts/<int:pk>/like', views.PostLikeAPIView.as_view(), name='post-detail'),
-    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,24 +1,8 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.response import Response
 
-from api.models import Post, Tag, Category, Comment
-from api.serializers import UserSerializer, PostSerializer, TagSerializer, CategorySerializer, CommentSerializer, \
-    PostListSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
+from api.models import Post
+from api.serializers import PostListSerializer
 
 class PostPageNumberPagination(PageNumberPagination):
     page_size = 3
@@ -28,30 +12,4 @@ class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
     pagination_class = PostPageNumberPagination
-
-
-class PostLikeAPIView(GenericAPIView):
-    queryset = Post.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.like += 1
-        instance.save()
-        return Response(instance.like)
-
-
-class TagViewSet(viewsets.ModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
-
-
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
 
