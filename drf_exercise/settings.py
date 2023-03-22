aab56fd87930807ef63f56ec14407ac2c3c43d01
rla_thdus <0228ksy@naver.com>
2023-03-22 21:31:02 +0900

    Chore: 날아가는 sql 확인 가능하게 설정

--- drf_exercise/settings.py
+++ drf_exercise/settings.py
@@ -138,3 +138,20 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 2,
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
