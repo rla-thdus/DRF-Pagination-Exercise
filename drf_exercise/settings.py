c5ba2a1baa912ee0b1cba6092aa8ce261469f439
rla_thdus <0228ksy@naver.com>
2023-04-01 18:17:35 +0900

    Chore: static file 연결

--- drf_exercise/settings.py
+++ drf_exercise/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = '/var/www/html/static'
 
 
 # Quick-start development settings - unsuitable for production
